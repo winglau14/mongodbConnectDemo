ce79beb8957a767c370c616e9adee4e48b8ed9e3
winglau14 <397069329@qq.com>
2019-01-12 18:52:02 +0800

    代码更新

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var bodyParser = require("body-parser");
 var app = express();
 var router = express.Router();
 //数据库链接
-var db = mongoose.connect("mongodb://localhost:27017/winglau14");
+mongoose.Promise = global.Promise;
+var db = mongoose.connect("mongodb://localhost:27017/userdb");
 db.connection.on("error", function (error) {
     console.log("数据库连接失败：" + error);
 });
@@ -13,29 +14,25 @@ db.connection.on("open", function () {
     console.log("------数据库连接成功！------");
 });
 //创建一个Schema  每一个schema会一一对应mongo中的collection
-var Schema = db.Schema;
-//实例化一个Schema
-var UserSchema = new Schema({
+var UserSchema = new mongoose.Schema({
     //设置user信息的数据格式
-    userName: {
-        type: String
-    },
-    passWord: {
-        type: String
-    }
-}, {
-    versionKey: false
+    userName: String,
+    passWord:String
 });
 //生成一个具体user的model
-var User = db.model("User", UserSchema, "user");
+var User = mongoose.model("userData",UserSchema);
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+});
 //get查询数据
 router.get("/user", function (req, res) {
     var type = req.query.type;
+    console.log(type);
     User.find({
-        type: type
+        userName: type
     }, function (err, result) {
         if (err) {
             console.log("Error:" + err);
@@ -46,7 +43,25 @@ router.get("/user", function (req, res) {
         }
     });
 });
-
+//新增数据
+router.get("/add", function (req, res) {
+    var userName = req.query.userName;
+    var passWord = req.query.passWord;
+    var newUser = new User({
+        userName: userName,
+        passWord:passWord
+    });
+    //保存数据
+    newUser.save(function (err, result) {
+        if (err) {
+            console.log("Error:" + err);
+        }
+        else {
+            console.log("result:" + result);
+            res.send(JSON.stringify(result));
+        }
+    });
+});
 //post查询数据
 router.post("/login", function (req, res) {
     User.find(req.body, function (err, result) {
@@ -60,4 +75,6 @@ router.post("/login", function (req, res) {
     });
 });
 app.use(router);
-app.listen(3001);
+app.listen(3001,function(){
+    console.log('listen port:3001');
+});
